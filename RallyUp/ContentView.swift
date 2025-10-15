ad45721cf591f1c60a7ab4965ed7209cdc95af19
Tom Parkinson <tomparkinson.97@icloud.com>
2025-10-14 20:51:36 -0400

    Initial Commit

---
+++ RallyUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RallyUp
+//
+//  Created by Tom Parkinson on 10/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RallyUp/RallyUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyUpApp.swift
+//  RallyUp
+//
+//  Created by Tom Parkinson on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct RallyUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
